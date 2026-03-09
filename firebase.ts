16f11dbf3433de1644320fd1fa9f48f6cd46a7f8
javadeveloperarpit <arpitmishraqq1801@gmail.com>
2026-03-10 01:05:09 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,18 +1,13 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import dotenv from "dotenv"
+import admin from "firebase-admin"
 
-dotenv.config()
-
-const firebaseConfig = {
- apiKey: process.env.FIREBASE_API_KEY,
- authDomain: process.env.FIREBASE_AUTH_DOMAIN,
- projectId: process.env.FIREBASE_PROJECT_ID,
- storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
- messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
- appId: process.env.FIREBASE_APP_ID
+if (!admin.apps.length) {
+ admin.initializeApp({
+  credential: admin.credential.cert({
+   projectId: process.env.FIREBASE_PROJECT_ID,
+   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g,"\n")
+  })
+ })
 }
 
-const app = initializeApp(firebaseConfig)
-
-export const db = getFirestore(app)
+export const db = admin.firestore()
