d576eefc57645382a2c01d5937cdcb194d51711a
unknown <arpitmishraqq1801@gmail.com>
2026-08-01 09:45:26 +0530

    Configure serverExternalPackages for firebase-admin

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "172.16.224.103",
+  ],
 
-  allowedDevOrigins:[
-    "172.16.224.103"
-  ]
-
+  serverExternalPackages: [
+    "firebase-admin",
+  ],
 };
-
 
 export default nextConfig;
