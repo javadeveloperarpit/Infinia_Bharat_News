838790722658dd3be388a1a008e87d87c8881cf0
primebharatnews <Primebharatnews1@gmail.com>
2026-08-19 18:14:38 +0530

    l

--- src/components/home/shorts-section.tsx
+++ src/components/home/shorts-section.tsx
@@ -628,7 +628,7 @@ export default function ShortsSection({
                   }
                 }}
                 tabIndex={0}
-                role="link"
+      
                 aria-label={`Open ${
                   short.title || "reel"
                 }`}
